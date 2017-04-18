ae3c677166c2fea4c82519ea65def26a91175e0b
absha <absha@DESKTOP-0KL3UBP>
2017-04-18 21:53:49 +0200

    Implementing the addToBack and removeBack with testing

--- src/Abuobayda/Shabat/DataStructure/DList.java
+++ src/Abuobayda/Shabat/DataStructure/DList.java
@@ -42,63 +42,51 @@ public class DList<T> implements DListInterface<T> {
         T value = null;
         if(!isEmpty())
         {
-            value = (T) head.getItem();
-            numOfItem--;
-            if(head == tail){   
+            if(head == tail){ 
+                value = (T) head.getItem();  
                 head=tail= null;
             }
             else{
+                value = (T) head.getItem();
                 head = head.getNext();
                 head.setPrev(null);
             }
+            numOfItem--;
         }
         return value;
     }
 
     @Override
     public void addToBack(T item) {
-        ListNode current = head;
-        // we start with one because we don't want the 
-        // current to become null before adding
-        int cnt = 1;
-        while(current!=null && cnt < numOfItem){
-            current = current.getNext();
-            cnt++;
-        }
-        
-        if (current!=null){
-            ListNode newNode = new ListNode(item);
-            current.setNext(newNode);
+        if(!isEmpty()){
+            ListNode temp = new ListNode(item, null, tail);
+            tail.setNext(temp);
+            tail =temp;
             numOfItem++;
             
-        }
-        //when the list is empty
-        else{
+        }else{
             addToFront(item);
         }
+
+
     }
 
     @Override
     public T removeBack() {
         T value = null;
         if(!isEmpty()){
-            ListNode current = head;
-            //to make sure the current will point to the node before the last node
-            int cnt = 2;
-            while(current!=null && cnt <numOfItem)
-            {
-                current = current.getNext();
-                cnt++;
+            if(head==tail){
+                value = (T) head.getItem();  
+                head=tail= null;
             }
-            
-            if(current!=null)
-            {
-                //current is pointing to the node before the last node
-                value = (T) current.getNext().getItem();
-                current.setNext(null);
-            }  
+            else{
+                value = (T) tail.getItem();
+                tail = tail.getPrev();
+                tail.setNext(null);
+                numOfItem--;
+            }
+             
         }
-        numOfItem--;
         return value;
     }
     
--- src/Abuobayda/Shabat/DataStructure/MainClass.java
+++ src/Abuobayda/Shabat/DataStructure/MainClass.java
@@ -30,7 +30,7 @@ public class MainClass {
         int val2 = myList.removeFront();
         System.out.println("Value of the removed node: "+ val2);
         myList.display();
-        /**
+        
         System.out.println("==========Testing addToBack method===============");
         myList.addToBack(10);
         myList.addToBack(20);
@@ -38,10 +38,21 @@ public class MainClass {
         myList.display();
         
         System.out.println("==============Testing remove Back================");
-        int val = myList.removeBack();
+        int val3 = myList.removeBack();
         myList.display();
-        System.out.println("value of the  removed node: "+ val);
+        System.out.println("value of the  removed node: "+ val3);
+        int val4 = myList.removeBack();
+        myList.display();
+        System.out.println("value of the  removed node: "+ val4);
+        int val5 = myList.removeBack();
+        myList.display();
+        System.out.println("value of the  removed node: "+ val5);
+        myList.addToBack(10);
+        myList.display();
+
         
+        
+        /**
         System.out.println("=========Testing numOfItems method========");
         System.out.println("Number Of Item in the List: "+ myList.numOfItems());
         
